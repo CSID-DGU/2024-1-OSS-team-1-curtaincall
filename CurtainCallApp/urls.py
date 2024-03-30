344ba3f4ff35796c6a8744e187f1850b160805fd
trv449 <donghyun0215@naver.com>
2024-03-30 12:14:52 +0900

    feat : swagger add

---
+++ CurtainCallApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+#from rest_framework.routers import DefaultRouter
+from CurtainCallApp.views import TestView
+
+# Create a router and register our viewsets with it.
+# router = DefaultRouter()
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('v1/test/', TestView.as_view(), name='test'),
+]
