0360a9b3615a09cd8fb7a3e38cf0c58939ec349a
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-03-30 23:14:26 +0900

    feat: added superadmin

--- CurtainCallApp/admin.py
+++ CurtainCallApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import File
 # Register your models here.
+
+#class FileAdmin(admin.ModelAdmin):
+#   search_fields = ['file']
+
+admin.site.register(File)
