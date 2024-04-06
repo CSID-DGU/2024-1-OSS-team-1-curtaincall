ec766c4b14bb1fe09dd0731e128a429fd0e79ec3
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-04-06 09:22:56 +0900

    feat: admin.py model import

--- CurtainCallApp/admin.py
+++ CurtainCallApp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import File
+from .models import *
 # Register your models here.
 
 #class FileAdmin(admin.ModelAdmin):
 #   search_fields = ['file']
 
-admin.site.register(File)
+admin.site.register(Stage)
+admin.site.register(Photo)
