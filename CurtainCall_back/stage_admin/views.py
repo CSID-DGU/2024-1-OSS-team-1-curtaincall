4033d021968331023778b5c687dceb2759b8d0b5
osc010524 <osc010524@gmail.com>
2024-04-14 20:30:28 +0900

    feat : 스테이지 개설 API 개발 완료

--- CurtainCall_back/stage_admin/views.py
+++ CurtainCall_back/stage_admin/views.py
@@ -2,21 +2,37 @@ from rest_framework import viewsets, permissions, generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from .models import MyModel, Stage_list, User_list
 
 import boto3
 import uuid
 
 import CurtainCall.settings as settings
 
+
 class helloAPI(APIView):
     def get(self, request):
         return Response("hello, world!")
 
-class mack_stage(APIView):
+
+class creatStage(APIView):
+    """
+    스테이지 생성 후 스테이지 ID 반환
+    """
+    @swagger_auto_schema(
+        request_body=(openapi.Schema(type=openapi.TYPE_OBJECT, properties={
+            'name': openapi.Schema(type=openapi.TYPE_STRING, description='호스트 명')
+        })),
+        responses={200: openapi.Schema(type=openapi.TYPE_OBJECT, properties={
+            'stageId': openapi.Schema(type=openapi.TYPE_STRING, description='스테이지 ID')
+        })})
     def post(self, request):
+
         # 1 input data
         data = request.data
-        folderName = data.get('folderName')
+        name = data.get('name')
 
         # 2 name preprocessing
         db_id = uuid.uuid4()
@@ -27,27 +43,23 @@ class mack_stage(APIView):
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
         )
 
-        s3_client  = session.client('s3')
+        s3_client = session.client('s3')
         bucket_name = settings.AWS_STORAGE_BUCKET_NAME
-        folder_key = db_id + '/'
+        folder_key = str(db_id) + '/'
 
-        response = s3_client.put_object(
-            Bucket=bucket_name,
-            Key=folder_key
-        )
-
-        # 응답 확인
-        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-            pass
-            # print(f"폴더 '{folder_name}'가 성공적으로 생성되었습니다.")
-        else:
-            # print(f"폴더 생성에 실패했습니다. 응답 코드: {response['ResponseMetadata']['HTTPStatusCode']}")
+        try:
+            # s3 제작 예외처리 부
+            s3_client.put_object(Bucket=bucket_name, Key=(folder_key))
+        except Exception as e:
             pass
 
         # 4 save db
-        # 데이터 베이스 제작 협의 필요
+        new_stage = Stage_list.create(db_id, name)
+        new_stage.save()
+        new_user_list = User_list.create(new_stage, name)
+        new_user_list.save()
 
+        # 5 response
+        request = {"stageId": db_id}
         # send response
-        return Response(db_id, status=status.HTTP_201_CREATED)
-
-
+        return Response(request, status=status.HTTP_200_OK)
