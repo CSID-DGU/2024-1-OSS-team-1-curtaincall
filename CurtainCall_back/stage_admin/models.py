efa9a83058dd9493a8292e34c2686d2ab3f19920
osc010524 <osc010524@gmail.com>
2024-04-14 20:31:57 +0900

    Revert "feat :"
    This reverts commit dd285278890e2b01c8fcd1c492d2bbda66907da3.

--- CurtainCall_back/stage_admin/models.py
+++ CurtainCall_back/stage_admin/models.py
@@ -1,29 +1,5 @@
-import uuid
 from django.db import models
 
 class MyModel(models.Model):
     name = models.CharField(max_length=100)
 # Create your models here.
-
-class Stage_list(models.Model):
-    app_label = 'stage_admin'
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    host = models.CharField(max_length=100)
-
-    @classmethod
-    def create(cls, id_val, host_val):
-        return cls(id=id_val, host=host_val)
-
-    def __str__(self):
-        return str(self.id)
-
-class User_list(models.Model):
-    app_label = 'stage_admin'
-    stage_id = models.ForeignKey(Stage_list, on_delete=models.CASCADE)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.CharField(max_length=100)
-
-    @classmethod
-    def create(cls,stage_id_val,user_val):
-        id = uuid.uuid4()
-        return cls(stage_id=stage_id_val, id=id, user=user_val)
