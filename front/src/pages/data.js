454083337ec7dc7b0bbb8ba3c19ceacd55e05948
JANG HYO JE <113086153+Dwawawawa@users.noreply.github.com>
2024-04-15 17:54:46 +0900

    feat : Add 8 pages - Start, Host, Guest, Upload, data, Sort, Quater, Choose

---
+++ front/src/pages/data.js
@@ -0,0 +1,30 @@
+let data = 
+[
+    {
+        id:0,
+        src : "https://i.namu.wiki/i/vffbNmI7-PbYBpzdflYGuEzt5k73rkM_tTfCPyKskgNolPHFZrzYAHLMcw4v-iaGjNfRXFlIV-X9_7kwfAJ_RFVQlBln27OZCWal5Duh15UOofzASXA9oy2GQBxx1hLT5yJBHD4luSj8uhaun73kaw.webp"
+    },
+    {
+        id:1,
+        src: "https://i.namu.wiki/i/YpsNG06XD07WNe8G0X9nOolbjzrz27lNv_r0gqDGcOTsZ5y8Kj5S5YXf4KwKDPeLAgnKMwQP_UUdJ84nfchbaNeNhX358hVCYDM5CFi16N8I4uLt3IioDdzoJvNdLc8--3gjKEebXeGP5M-_8vEmOg.webp"
+    },
+    {
+        id:2,
+        src: "https://i.namu.wiki/i/2pQvC1M_KpG0W_QRhy64newGeFgh49cDnDVopvviyTdKCupJ2qNkTpWG6nVkSoIzOOaH_raaJhRWzFaa-GoWei_THf_U2zD-PDenPSh-MEEwV6LWVB9ZRvd4_y8wDDs14ngdhYYH0iqmS-F--ZLJUA.webp"
+    },
+    {
+        id:3,
+        src: "https://i.namu.wiki/i/Sgg6AHqTXnLWYmfJYR5ma_YzKyM-26mXS1Kvo7sZfRniPXKPvSHATqUKKbJcctX629UbabaomfuCdOEMa6pAicRnOFuUidage5TDFzYAW1I00KYtsTZPxekG0Z8q5MEEBNKGsLRe6ABAIeRtooxJYg.webp"
+    },
+    {
+        id:4,
+        src: "https://i.namu.wiki/i/Ox2uwo_HoQtYG_RNtsijVYYYXqx1diowLa9B72yF1rDsHEOyBI6zPNaii8BBq3YmS7go1RYLFCdUpK6_WyMpLADHVX6eE-68yjjXuOqCQydkwb5XwHzM99Vh-E7nmegHGL7Z7PTLj4ae4HlG5E4yRA.webp"
+    },
+    {
+        id:5,
+        src: "https://i.namu.wiki/i/hU9NKWUsqln0hDwqMKxU1CnKN-8hq9eWhQOrqAoLktFxNlBKIUsJBPUBvsNIwtNMegIgW-gKKKHj1IQN0syZ0nRUoT73VUNE6fM3fwda-vQwk3LRnT_DPvqDKpIJd2OYNIG4u2WtFPegiPWSdAiUrg.webp"
+    }
+
+]
+
+export default data;
