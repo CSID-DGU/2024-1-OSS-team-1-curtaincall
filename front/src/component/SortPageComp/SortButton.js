629419d55d5a90138939eb2770550d6320594a95
trv449 <donghyun0215@naver.com>
2024-05-18 13:41:47 +0900

    Style : sort페이지 컴포넌트 분리

---
+++ front/src/component/SortPageComp/SortButton.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Button, ThemeProvider, CircularProgress } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { SBTheme } from './Theme/SBTheme';
+import localuri from '../../pages/localuri.js';
+
+const SortButton = ({ children }) => {
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        if (loading) return;
+        setLoading(true);
+
+        fetch('http://' + localuri + ':8000/Algorithm_cv2/test/', {
+            method: 'GET',
+        })
+            .then(response => {
+                setLoading(false);
+                if (response.ok) {
+                    navigate('/quater');
+                } else {
+                    throw new Error('Something went wrong during the upload.');
+                }
+            })
+            .catch(error => {
+                setLoading(false);
+                console.error('An error occurred:', error);
+            });
+    };
+
+    return (
+        <ThemeProvider theme={SBTheme}>
+            <Button
+                onClick={handleClick}
+                disabled={loading}
+                sx={{
+                    backgroundColor: loading ? '#ffa793' : '#ff5838',
+                    color: 'white',
+                    '&:hover': {
+                        backgroundColor: loading ? '#ffa793' : '#ffa793'
+                    }
+                }}
+            >
+                {loading ? <CircularProgress size={24} style={{ color: 'white' }} /> : children}
+            </Button>
+        </ThemeProvider>
+    );
+};
+
+export default SortButton;
