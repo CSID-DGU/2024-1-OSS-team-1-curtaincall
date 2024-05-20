594d21ba16ada5a7c042573630b05360e2db6051
trv449 <donghyun0215@naver.com>
2024-05-20 16:16:26 +0900

    style : pixel단위를 동적으로 변경

--- front/src/components/.PublicTheme/ButtonTheme.jsx
+++ front/src/components/.PublicTheme/ButtonTheme.jsx
@@ -5,17 +5,22 @@ export const ButtonTheme = createTheme({
         MuiButton: {
             styleOverrides: {
                 root: {
-                    width: '33.33%',
-                    height: '70px',
+                    width: '33.33vw',
+                    height: '8vh',
                     borderRadius: '50px',
                     fontWeight: 'bold',
-                    fontSize: '1.25rem',
+                    fontSize: '1.25vw',
                     position: 'relative',
                     transition: 'background-color 0.5s ease',
-                    marginLeft: '0.25%',
+                    marginLeft: '0.25vw',
+                    '@media (max-width: 1000px)': {
+                        fontSize: '2.0vw',
+                        marginTop: '0.25vw',
+                    },
                     '@media (max-width: 768px)': {
-                        width: '66.6%',
-                        marginTop: '0.25%',
+                        fontSize: '2.0vw',
+                        width: '66.6vw',
+                        marginTop: '0.25vw',
                     }
                 }
             }
--- front/src/components/.PublicTheme/InputForm.jsx
+++ front/src/components/.PublicTheme/InputForm.jsx
@@ -5,17 +5,17 @@ export const InputFormTheme = createTheme({
         MuiOutlinedInput: {
             styleOverrides: {
                 root: {
-                    width: '66.66%',
-                    height: '70px',
+                    width: '66.66vw',
+                    height: '8vh',
                     borderRadius: '50px',
                     fontWeight: 'bold',
-                    fontSize: '1.25rem',
+                    fontSize: '1.25vw',
                     position: 'relative',
                     transition: 'background-color 0.5s ease',
                     margin: 'auto',
-                    marginTop: '0.25%',
+                    marginTop: '0.25vw',
                     '@media (max-width: 768px)': {
-                        width: '100%',
+                        width: '100vw',
                     },
                 },
                 notchedOutline: {
@@ -27,7 +27,10 @@ export const InputFormTheme = createTheme({
             styleOverrides: {
                 input: {
                     fontWeight: 'bold',
-                    fontSize: '1.25rem',
+                    fontSize: '1.25vw',
+                    '@media (max-width: 1000px)': {
+                        fontSize: '2.0vw',
+                    },
                 }
             }
         }
