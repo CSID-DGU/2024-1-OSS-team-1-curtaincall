96dbd3fe4dc773d116807606e55fd0b34ee77754
trv449 <donghyun0215@naver.com>
2024-05-22 01:22:52 +0900

    feat : checkstageusers api 연결, createStage api 연결, joinStage api 연결

--- front/src/pages/Host.jsx
+++ front/src/pages/Host.jsx
@@ -1,20 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import guestData from './data.jsx'; // 실제 데이터 파일은 어떨까?
 import CopyButton from '../components/HostPageComp/CopyButton';
 import GoToUploadButton from '../components/HostPageComp/GoToUploadButton';
-import GuestList from "../components/HostPageComp/GuestList";
+import GuestList from '../components/HostPageComp/GuestList';
+import api from '../axios';
 
 function Host() {
     const navigate = useNavigate();
     const [url, setUrl] = useState('');
-    const [guests, setGuests] = useState(guestData); // guestData가 배열이라고 가정
+    const [guests, setGuests] = useState([]);
 
     // 무작위 URL 생성 함수
     useEffect(() => {
         const randomUrl = 'https://yourapp.com/' + Math.random().toString(36).substring(2, 15);
         setUrl(randomUrl);
+    }, []);
+
+    useEffect(() => {
+        const fetchGuests = async () => {
+            try {
+                const response = await api.get('/checkStageUsers/', {
+                    params: {
+                        stageId: 0 // 실제 스테이지 ID를 설정
+                    }
+                });
+
+                if (response.status === 200) {
+                    const guestData = response.data.users.map(user => ({
+                        id: user.user.id,
+                        name: user.user.name,
+                        sendImage: user.sendImage
+                    }));
+                    setGuests(guestData);
+                } else {
+                    console.error('Failed to fetch guests');
+                }
+            } catch (error) {
+                console.error('Error fetching guests:', error);
+            }
+        };
+
+        fetchGuests();
     }, []);
 
     return (
