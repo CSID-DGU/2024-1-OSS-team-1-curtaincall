57f77080cd12c58ecb37f89d00130527519260da
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-05-25 23:37:56 +0900

    feat: added oauth structure

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Container } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { GoogleLogin } from '@react-oauth/google';
 import LoginButton from '../components/LoginPageComp/LoginButton'; // Adjust the path accordingly
 import UsernameInputForm from '../components/LoginPageComp/IDInputForm'; // Adjust the path accordingly
 import PasswordInputForm from '../components/LoginPageComp/PasswordInputForm'; // Adjust the path accordingly
+import api from '../axios'
 
 function Login() {
     const navigate = useNavigate();
@@ -15,6 +17,23 @@ function Login() {
         navigate('/');
     };
 
+    const handleGoogleLoginSuccess = async (response) => {
+        console.log('Google login successful:', response);
+        try {
+            const res = await api.post('/auth/complete/google-oauth2/', {
+                access_token: response.credential,
+                state: response.state  // state 매개변수 포함
+            });
+            if (res.data.key) {
+                navigate('/');
+            } else {
+                console.error('Login failed:', res.data.message);
+            }
+        } catch (error) {
+            console.error('Error logging in:', error);
+        }
+    };
+
     return (
         <Container>
             <br/>
@@ -23,6 +42,11 @@ function Login() {
             <PasswordInputForm password={password} onPasswordChange={setPassword} />
             <br/>
             <LoginButton onClick={handleLogin}>Login</LoginButton>
+            <br/>
+            <GoogleLogin
+                onSuccess={handleGoogleLoginSuccess}
+                onFailure={(response) => console.error('Google login failed:', response)}
+            />
         </Container>
     );
 }
