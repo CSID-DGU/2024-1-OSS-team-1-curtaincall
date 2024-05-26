c18673dcb44f6831e39a5059e492f3e8050bbd9e
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-05-26 23:38:53 +0900

    feat: fetching from S3 updated

--- CurtainCall_back/Algorithm_cv2/views.py
+++ CurtainCall_back/Algorithm_cv2/views.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 
 class AlgoView(APIView):
     permission_classes = [permissions.AllowAny]
-
     def get(self, request):
-        return Response(sp.getsim())
+        return Response(sp.getsim(stage_id=request.user.stage_uuid_id), status=status.HTTP_200_OK)
 # Create your views here.
