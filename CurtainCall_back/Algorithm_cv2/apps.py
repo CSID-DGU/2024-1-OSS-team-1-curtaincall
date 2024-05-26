f9be5983ccc49e3623f768179e80305b677d0d23
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-05-26 23:39:59 +0900

    feat: now model loading only occurs once

--- CurtainCall_back/Algorithm_cv2/apps.py
+++ CurtainCall_back/Algorithm_cv2/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class AlgorithmCv2Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Algorithm_cv2'
+
+    def ready(self):
+        from .sim_print import load_model
+        load_model()
