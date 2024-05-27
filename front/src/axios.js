3e844c9ecbcf3a995fe35ea2d432146f9c2b4a6c
osc010524 <osc010524@gmail.com>
2024-05-27 13:14:32 +0900

    fix : 외부 접속 IP 허용 설정 변경 (프론트) baseURL: 'http://127.0.0.1:8000', => 0.0.0.0

--- front/src/axios.js
+++ front/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000', //API의 기본 URL 설정
+    baseURL: 'http://0.0.0.0:8000', //API의 기본 URL 설정
     headers: {
         'Content-Type': 'application/json'
     },
