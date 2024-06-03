88bee6208973924713f21c94f1ac44033113e438
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-06-03 10:32:08 +0900

    feat: container design added

--- front/src/components/.PublicTheme/ContainerTheme.jsx
+++ front/src/components/.PublicTheme/ContainerTheme.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Container, CssBaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
+import backgroundDots from '../../img/background_dots.png'; // 이미지 파일 경로
 
 export const ContainerTheme = createTheme({
     components: {
         MuiContainer: {
             styleOverrides: {
-                root: {
-                    backgroundColor: '#b2b2b2',
-                    height: '100vh',
+                root: { //'#012B28'
+                    backgroundColor: '#F5F5F5',
+                    //backgroundImage: `url(${backgroundDots})`,
+                    //backgroundSize: 'auto', // 필요에 따라 'contain' 또는 'cover'로 변경 가능
+                    //backgroundSize: '25px 25px',
+                    //backgroundRepeat: 'repeat',
+                    //backgroundPosition: 'center', // 필요에 따라 위치 조정 가능
+                    height: '100%',
                     maxWidth: 'none',
                     //여긴 그냥 CSS랑 똑같음
                 },
--- front/src/components/CustomContainer.jsx
+++ front/src/components/CustomContainer.jsx
@@ -1,14 +1,41 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { ThemeProvider } from '@mui/material/styles';
 import { ContainerTheme } from './.PublicTheme/ContainerTheme';
+import { useRecoilValue } from 'recoil';
 
-export default function CustomContainer({children}) {
+import {
+    useMediaQuery,
+    useTheme,
+} from "@mui/material";
+
+export default function CustomContainer({ children, style }) {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+    const flexDirection = isMobile ? 'column': 'row';
+    const justifyContent = isMobile ? 'flex-start' : 'space-evenly';
+    const alignItems = isMobile ? 'center' : 'flex-start';
+
     return (
         <ThemeProvider theme={ContainerTheme}>
-            <Container style={{ width: '100vw', maxWidth: 'none', padding: 0 }}>
+            <CssBaseline />
+            <Container
+                maxWidth={false} // maxWidth를 false로 설정하여 기본 마진을 제거
+                disableGutters // 기본 패딩을 제거
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    padding: '4px',
+                    marginTop: '0px',
+                    display: 'flex',
+                    flexDirection: flexDirection,
+                    justifyContent: justifyContent,
+                    alignItems: alignItems,
+                    minHeight: '100%',
+                    ...style
+                }}
+            >
                 {children}
             </Container>
         </ThemeProvider>
