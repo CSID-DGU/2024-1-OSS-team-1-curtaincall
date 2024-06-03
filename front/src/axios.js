fef4671f593d8834bc3e469e81628841a69e24c6
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-06-03 10:18:02 +0900

    fix: server address updated

--- front/src/axios.js
+++ front/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL: 'http://54.180.118.53:8000',
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'http://54.180.118.53:8000',
+    //baseURL: 'http://127.0.0.1:8000',
     headers: {
         'Content-Type': 'application/json'
     },
