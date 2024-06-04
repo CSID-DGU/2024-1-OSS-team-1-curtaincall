a41672720f01953bfe8a8f2c585e61165c678a4d
trv449 <donghyun0215@naver.com>
2024-06-04 22:27:07 +0900

    feat : 더미데이터 클릭 불가능

--- front/src/components/QuarterPageComp/RoundContainer.jsx
+++ front/src/components/QuarterPageComp/RoundContainer.jsx
@@ -7,31 +7,39 @@ const RoundContainer = ({ group, onImageSelect }) => {
 
     return (
         <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, overflow: 'hidden' }}>
-        <div style={{ width: '100vw', height: '70vh', margin: 0, padding: 0, overflow: 'hidden' }}>
-            <Grid container spacing={0} style={{ height: '100%', margin: 0, width: '100%' }}>
-                {group.images.map((image, idx) => (
-                    <Grid
-                        item
-                        xs={6} // 2x2 layout for mobile (<= 900px)
-                        md={3} // 4x1 layout for desktop (> 900px)
-                        style={{ height: isMobile ? '50%' : '100%', margin: 0, padding: 0 }} // 조건부 높이 설정
-                        key={idx}
-
-                    >
-                        <Card style={{ height: '100%', margin: 0 }}>
-                            <CardActionArea style={{ height: '100%' }} onClick={() => onImageSelect(image, group.group_id)}>
-                                <CardMedia
-                                    component="img"
-                                    alt={`Image ${idx}`}
-                                    style={{ height: '100%', width: '100%', objectFit: 'contain' }}
-                                    image={image.src}
-                                />
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-        </div>
+            <div style={{ width: '100vw', height: '70vh', margin: 0, padding: 0, overflow: 'hidden' }}>
+                <Grid container spacing={0} style={{ height: '100%', margin: 0, width: '100%' }}>
+                    {group.images.map((image, idx) => (
+                        <Grid
+                            item
+                            xs={6} // 2x2 layout for mobile (<= 900px)
+                            md={3} // 4x1 layout for desktop (> 900px)
+                            style={{ height: isMobile ? '50%' : '100%', margin: 0, padding: 0 }} // 조건부 높이 설정
+                            key={idx}
+                        >
+                            <Card style={{ height: '100%', margin: 0 }}>
+                                {image.isDummy ? (
+                                    <CardMedia
+                                        component="img"
+                                        alt={`Image ${idx}`}
+                                        style={{ height: '100%', width: '100%', objectFit: 'contain' }}
+                                        image={image.src}
+                                    />
+                                ) : (
+                                    <CardActionArea style={{ height: '100%' }} onClick={() => onImageSelect(image, group.group_id)}>
+                                        <CardMedia
+                                            component="img"
+                                            alt={`Image ${idx}`}
+                                            style={{ height: '100%', width: '100%', objectFit: 'contain' }}
+                                            image={image.src}
+                                        />
+                                    </CardActionArea>
+                                )}
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </div>
         </div>
     );
 };
