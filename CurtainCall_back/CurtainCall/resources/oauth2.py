c70dfcc160baa6beece9a06e0973087660b8d468
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-06-06 18:39:07 +0900

    Revert "refactor: .env refactored"
    This reverts commit f63fdaee9ffee42c13f778ebd453aa3940731c31.

--- CurtainCall_back/CurtainCall/resources/oauth2.py
+++
@@ -1,4 +0,0 @@
-import os
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '614742490862-7dharreoq95g6culheb963uoum3upk1c.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('OAUTH_SECRET')
--- CurtainCall_back/CurtainCall/resources/postgre.py
+++
@@ -1,7 +0,0 @@
-import os
-
-database_name = 'curtaincall'
-database_user = 'postgres'
-PASSWORD = os.getenv('POSTGRE_PASSWORD')
-HOST = os.getenv('POSTGRE_HOST')
-PORT = os.getenv('POSTGRE_PORT')
