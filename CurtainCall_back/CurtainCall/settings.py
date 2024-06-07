eed34c09b78ff069169c5342c5d1888b45a72aab
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-06-07 21:48:30 +0900

    chore: token lifetime extended

--- CurtainCall_back/CurtainCall/settings.py
+++ CurtainCall_back/CurtainCall/settings.py
@@ -116,7 +116,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=59),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
